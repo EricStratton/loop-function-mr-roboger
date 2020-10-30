7af2ff49396e52cb900a3ab983885d5a6f06bf0c
Eric Stratton <strattonericj@gmail.com>
2020-10-30 11:43:16 -0700

    not add anything, attempting to make a function that works with user input but so far unsuccesful

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 // Business Logic //
+// let blank = [];
+// function beepBoop (number) {
+//   for(let i = 0; i <= number; i++) {
+//     return number[i];
+//   }
+// };
 
-function beepBoop (number) {
-  for(let i = 0; i <= number; i++) {
-    return number[i];
-  }
+
+function beepBoop(userNumber, result) {
+  return userNumber * 2;
 };
+
+
 
 // User Interface Logic //
 
-$(document).ready(function(){
-  $("form#userInput").submit(function(){
-    let number = parseInt($("#input").val());
+$(document).ready(function() {
+  $("form#userInput").submit(function() {
+    let userNumber = parseInt($("#input").val());
+ 
+    const result = beepBoop (userNumber);
+
   });
 });
