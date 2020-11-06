53367a88008a6473860ce7b8a83bfb4bba012f0f
Eric Stratton <strattonericj@gmail.com>
2020-11-05 17:44:15 -0800

    refactored code more, got rid of limit, shorter commit descrip

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,16 @@
 function beepBoop(userNumber) {
   let emptyArray = [];
   for (let i = 0; i <= userNumber; i++) {
-    emptyArray.push(i);
-    if (i < 100) {
-      if (i === 3 || i % 10 === 3) {
-        emptyArray[i] = " Will you be my neighbor?";
-      } else if (i === 2 || i % 10 === 2) {
-        emptyArray[i] = " Boop!";
-      } else if (i === 1) {
-        emptyArray[i] = " Beep!";
-      }
-    } else {
-      $("#too-much").show();
-      break;
+    if (i === 3 || i % 10 === 3) {
+      emptyArray[i] = " Will you be my neighbor?";
+    } else if (i === 2 || i % 10 === 2) {
+      emptyArray[i] = " Boop!";
+    } else if (i === 1) {
+      emptyArray[i] = " Beep!";
     }
+  emptyArray.push(i);
   } 
-    return emptyArray;
+  return emptyArray;
 }
 
 // User Interface Logic //
@@ -31,7 +26,7 @@ $(document).ready(function() {
     const result = beepBoop(userNumber);
     
     $("#print").text(result);
-    $("#too-much").hide();
+    // $("#too-much").hide();
 
   });
 });
